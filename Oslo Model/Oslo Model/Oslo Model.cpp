06b134e537c8ee647a5e4695efc90968a2ee9df8
quantumqwah <mehistory92@gmail.com>
2014-02-18 23:08:24 +0000

    Complexity file with header and task 2a

--- Oslo Model/Oslo Model/Oslo Model.cpp
+++ Oslo Model/Oslo Model/Oslo Model.cpp
@@ -9,29 +9,36 @@ using namespace std;
   
 int main()
 {
-	const int L=50;
-	int latt[L]={};
-	int input=0;
 
-	for( int c=0 ; c < 10000 ; c++)
+	for (int l=2; l<100 ; l++)
 	{
-		 latt[0]++;
-	
-	for ( int i=0; i< L-1&&latt[i]!=0; i++)
-	{
-	  int zth= rand()%2+1;
+		vector<int> latt(l,0);
+		int input=0;
+
+		for( int c=0 ; c < 10000 ; c++)
+		{
+			 latt[0]++;
+	if(latt[l-1]!=0)
 	 
-	  int z= latt[i]-latt[i+1];
+		break;	
+		for ( int i=0; i< l-1&&latt[i]!=0; i++)
+			{
+			  int zth= rand()%2+1;
+	 
+			  int z= latt[i]-latt[i+1];
 
-	  if (z>zth)
-	  {
-		  latt[i]--;
-		  latt[i+1]++;
+			  if (z>zth)
+			  {
+				  latt[i]--;
+				  latt[i+1]++;
 
-	}
-	}
+			}
+			  
+		    }
+			
+			
 
-	for (int j=0 ; j <L-1&&latt[j]!=0; j++)
+	for (int j=0 ; j <l&&latt[j]!=0; j++)
 	{
 		cout << latt[j] << " ";
 	}
@@ -52,4 +59,4 @@ int main()
 
 
 	
-
+	}

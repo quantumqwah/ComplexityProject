faccd922b58df87bf591d2f6dd1a01dd460408e4
quantumqwah <mehistory92@gmail.com>
2014-02-19 17:46:58 +0000

    randomise threshold

--- Oslo Model/Oslo Model/Oslo Model.cpp
+++ Oslo Model/Oslo Model/Oslo Model.cpp
@@ -11,176 +11,95 @@ int z( vector<int> a, int i)
 }
 
 
-  
-void heightOslo()
-{
-
-	for (int l=2; l<100 ; l++)
-	{
-		vector<int> latt(l,0);
-		int input=0;
-
-		for( int c=0 ; c < 10000 ; c++)
-		{
-			 latt[0]++;
-	         if(latt[l]!=0)
-
-			 break;	
-		     for ( int i=0; i< l-1&&latt[i]!=0; i++)
-			  {
-			     int zth= rand()%2+1;
-
-			     int z= latt[i]-latt[i+1];
-
-			  if (z>zth)
-			  {
-				  latt[i]--;
-				  latt[i+1]++;
-
-			  }
-
-		    }
-
-
-
-	for (int j=0 ; j <l&&latt[j]!=0; j++)
-	{
-		cout << latt[j] << " ";
-	}
-		cout << endl;
-
-	if(input==0)
-	{
-	cin >> input;
-	}
-	input--;
-
-	}
-
-
-   
-} 
-    
-
-
-}
-
-void slopeOslo()
-{
-	int l=100;
-	vector<int> z(l,0);
-	int input=0;
-
-	for( int c=0 ; c < 10000 ; c++)
-	{
-	    int zth= rand()%2+1;
-		z[0]++;
-
-			if(z[0] >zth)
-			{
-				z[0]=z[0]-2;
-				z[1]=z[1]+1;
-
-				for( int i=1; i< l-2; l++)
-			   {
-						if(z[i] >zth)
-						{
-							z[i]=z[i]-2;
-							z[i+1]=z[i+1]+1;
-							z[i-1]=z[i-1]+1;
-						}
-
-			   }
-
-			}
-
-
-	  z[l-1]=z[l-1]-1;
-	  z[l-2]=z[l-2]+2;
-
-	  for (int j=0 ; j <l; j++)
-	{
-		cout << z[j] << " ";
-	}
-
-	}
-
-
-
-
-
-	/*if(input==0)
-	{
-	cin >> input;
-	}
-	input--;
-	*/
-	}
 
 
 
 int main()
 {
-	int l=40;
+	int l=10;
 	vector<int> z(l,0);
-	int input=0;
+	vector<int> zth(l, 0);
+		    int input=0;
+    
+	
 
-	for( int c=0 ; c < 10000 ; c++)
+	for( int t=0 ;t < 10000 ; t++)
 	{
-	    int zth= rand()%2+1;
+
 		z[0]=z[0]+1;
+		bool fullrelaxed=false;
+		int checkifrelax=0;
+		
+		while(fullrelaxed==false)
+		{
+			for (int i=0;i<l;i++) 
+	   {
 
-			if(z[0] > zth)
-			{
-				z[0]=z[0]-2;
-				z[1]=z[1]+1;
+		zth[i]=rand()%2+1;
 
-				for( int i=1; i<= l-2&& z[i]> zth; i++)
-			   {
-
+	     }
+			checkifrelax=0;	
+			if(z[0] > zth[0])
+				{
 				
-							z[i]=z[i]-2;
-							z[i+1]=z[i+1]+1;
-							z[i-1]=z[i-1]+1;
+					z[0]=z[0]-2;
+					z[1]=z[1]+1;
 
-							z[i];
-							z[i+1];
-
+					checkifrelax++;
+				}
+						
+					for( int i=1; i<= l-2; i++)
+			   {
+				
+				            if(z[i] > zth[i])
+							{
+								z[i]=z[i]-2;
+								z[i+1]=z[i+1]+1;
+								z[i-1]=z[i-1]+1;
+								checkifrelax++;
+							}
 					
 
 			   }
 
-						if( z[l-1]>zth)
-							{
-							z[l-1]=z[l-1]-1;
-						    z[l-2]=z[l-2]+1;
-							}
-				  for (int j=0; j<l; j++)
+				if( z[l-1] >zth[1-1])
+				{
+				      z[l-1]=z[l-1]-1;
+					  z[l-2]=z[l-2]+1;
+					  checkifrelax++;
+				}
+
+				
+			
+
+				if (checkifrelax==0)
+				{
+					fullrelaxed=true;
+				}
+				
+
+
+				  
+	
+
+				
+		}
+						  for (int j=0; j<l; j++)
 				  {
 					cout << z[j] << " ";
 				  }
 
 				  cout<< endl;
-	/*			  
+				 // cin.get();
+
+
+
 	if(input==0)
 	{
 	cin >> input;
 	}
-	input--;*/
+	input--;
 				}
-
-			}
-
-
-	
-
-
-
-
-
-
-
-
 }
 	
 
